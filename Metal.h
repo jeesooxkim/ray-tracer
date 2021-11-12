b5e2b8148180cca734585c469c266d12912b0fec
Jeesoo Kim <jeesooxkim@gmail.com>
2021-11-11 20:13:33 -0600

    fuzziness parameter

--- Metal.h
+++ Metal.h
@@ -6,17 +6,20 @@
 
 class Metal: public Material {
     public:
-        Metal(const Vec3& a) {
+        Metal(const Vec3& a, const float f) {
             albedo = a;
+            fuzz = f;
         }
         virtual bool scatter(const Ray& ray, HitRecord& rec, Vec3& attenuation, Ray& scattered) const {
             Vec3 reflected = reflect(unit_vector(ray.direction()), rec.normal);
-            scattered = Ray(rec.p, reflected);
+            Vec3 fuzzed = reflected + fuzz*random_in_unit_sphere();
+            scattered = Ray(rec.p, fuzzed);
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
 
         Vec3 albedo;
+        float fuzz;
 };
 
 #endif // METAL_H
--- main.cpp
+++ main.cpp
@@ -44,8 +44,8 @@ int main() {
 
     list[0] = new Sphere(Vec3(0, 0, -1), 0.5, new Lambertian(Vec3(0.8, 0.3, 0.3)));
     list[1] = new Sphere(Vec3(0,-100.5, -1), 100, new Lambertian(Vec3(0.8, 0.8, 0.0)));
-    list[2] = new Sphere(Vec3(1, 0, -1), 0.5, new Metal(Vec3(0.8, 0.6, 0.2)));
-    list[3] = new Sphere(Vec3(-1,0, -1), 0.5, new Metal(Vec3(0.8, 0.8, 0.8)));
+    list[2] = new Sphere(Vec3(1, 0, -1), 0.5, new Metal(Vec3(0.8, 0.6, 0.2), 1));
+    list[3] = new Sphere(Vec3(-1,0, -1), 0.5, new Metal(Vec3(0.8, 0.8, 0.8), 0.5));
     Hitable *world = new HitableList(list, listSize);
 
     // the rows are written out from top to bottom
